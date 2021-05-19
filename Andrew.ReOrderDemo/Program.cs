bee6d5e0560e3444fd6925744994df24292017da
Andrew Wu <chicken@chicken-house.net>
2021-05-20 04:08:23 +0800

    demo version

--- Andrew.ReOrderDemo/Program.cs
+++ Andrew.ReOrderDemo/Program.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Andrew.ReOrderDemo
 {
@@ -10,45 +11,44 @@ namespace Andrew.ReOrderDemo
     {
         static void Main(string[] args)
         {
-            foreach(var x in GetCommands())
-            {
-                Console.WriteLine($"{x.Position:#000}, {x.Message}; {x.OccurAt.TotalMilliseconds}");
-            }
-            return;
+            //foreach(var x in GetCommands(false))
+            //{
+            //    Console.WriteLine($"{x}");
+            //}
+            //return;  
 
-
-            var ro = new DemoReOrderBuffer(3);
-
-            foreach(var item in GetData())
+            var ro = new StupidReOrderBufferDemo(TimeSpan.FromMilliseconds(100));
+            foreach (var item in GetCommands(true))
             {
                 ro.Push(item);
             }
+            ro.Flush();
+            ro.DumpMetrics(Console.Out);
         }
 
-        static IEnumerable<int> GetData()
-        {
-            return new int[] { 3, 2, 1, 0, 4, 5, 9, 6, 7, 10, 8, 11, 12, 20, 19, 13, 14, 18, 17, 16, 15,30,29,28,27,26,25,24,23,22,21 };
-        }
 
         static IEnumerable<OrderedCommand> GetCommands(bool boost = true)
         {
-            //SortedSet<OrderedCommand> orders = new SortedSet<OrderedCommand>();
+            int total_count = 3000;
+            int cmd_period = 100;
+            int cmd_noise = 500;
+
+
             List<OrderedCommand> orders = new List<OrderedCommand>();
+            DateTime start = DateTime.Now;
 
             Random rnd = new Random();
-            for (int i = 1; i <= 100; i++)
+            for (int i = 0; i < total_count; i++)
             {
                 orders.Add(new OrderedCommand()
                 {
                     Position = i,
-                    OccurAt = TimeSpan.FromMilliseconds( 500 + i * 100 + rnd.Next(500) - 500/2),
+                    OccurAt = start + TimeSpan.FromMilliseconds(cmd_noise + i * cmd_period + rnd.Next(cmd_noise) - cmd_noise / 2),
                     Message = $"CMD-{i:#00000}"
                 });
             }
-            //orders.Sort((a, b) => a.OccurAt.CompareTo(b.OccurAt));
 
-            TimeSpan start = TimeSpan.Zero;
-            foreach(var c in (from x in orders orderby x.OccurAt ascending select x))
+            foreach (var c in (from x in orders orderby x.OccurAt ascending select x))
             {
                 if (!boost)
                 {
@@ -61,103 +61,119 @@ namespace Andrew.ReOrderDemo
     }
 
 
-    public class OrderedCommand// : IComparable<OrderedCommand>
+    public class OrderedCommand
     {
         public int Position = 0;
-        public TimeSpan OccurAt = TimeSpan.Zero;
+        public DateTime OccurAt = DateTime.MinValue;
         public string Message;
 
-        //public int CompareTo([AllowNull] OrderedCommand other)
-        //{
-        //    return this.Position.CompareTo(other.Position);
-        //}
+        public override string ToString()
+        {
+            return $"{this.Position:#000}, {this.Message}; {this.OccurAt:HH:mm:ss.fff}";
+        }
     }
 
 
     public abstract class ReOrderBufferBase
     {
-        protected int _buffer_size_limit = 0;
-        protected int _current_index = 0;
+        protected readonly TimeSpan _buffer_duration = TimeSpan.Zero;
 
 
-        protected ReOrderBufferBase(int bufferSize)
+        private int _metrics_total_push = 0;
+        private int _metrics_total_pop = 0;
+        private int _metrics_total_drop = 0;
+        private int _metrics_buffer_max = 0;
+
+
+        protected ReOrderBufferBase(TimeSpan buffer_duration_limit)
         {
-            this._buffer_size_limit = bufferSize;
-            this._current_index = 0;
+            this._buffer_duration = buffer_duration_limit;
         }
 
-        public abstract bool Push(int data);
 
-        public bool Pop(int data)
+        public bool Push(OrderedCommand data)
         {
-            //this._current_index = (data + 1);
+            this._metrics_total_push++;
+
+            int current_buffer_size = this._metrics_total_push - this._metrics_total_pop - this._metrics_total_drop;
+            this._metrics_buffer_max = Math.Max(current_buffer_size, this._metrics_buffer_max);
+
+            return this._Push(data);
+        }
+        public bool Flush()
+        {
+            return this._Flush();
+        }
+
+        protected abstract bool _Push(OrderedCommand data);
+        protected abstract bool _Flush();
+
+        public bool Pop(OrderedCommand data)
+        {
+            this._metrics_total_pop++;
             Console.WriteLine($"POP:  {data}");
             return true;
         }
-        public bool Skip(int data)
+        public bool Drop(OrderedCommand data, string reason)
         {
-            Console.WriteLine($"SKIP: {data}");
+            this._metrics_total_drop++;
+            Console.WriteLine($"DROP: {data} ({reason})");
+            return true;
+        }
+
+
+        public void DumpMetrics(TextWriter writer)
+        {
+            writer.WriteLine();
+            writer.WriteLine($"ReOrder Metrics:");
+            writer.WriteLine($"- total push: {this._metrics_total_push}");
+            writer.WriteLine($"- total pop:  {this._metrics_total_pop}");
+            writer.WriteLine($"- total drop: {this._metrics_total_drop}");
+            writer.WriteLine($"- drop rate: {this._metrics_total_drop * 100D / this._metrics_total_push:#.##} %");
+            writer.WriteLine($"- max buffer size: {this._metrics_buffer_max}");
+        }
+
+
+        public class OrderedCommandComparer : IComparer<OrderedCommand>
+        {
+            public int Compare([AllowNull] OrderedCommand x, [AllowNull] OrderedCommand y)
+            {
+                return x.Position.CompareTo(y.Position);
+            }
+        }
+    }
+
+
+
+    public class StupidReOrderBufferDemo : ReOrderBufferBase
+    {
+        private List<OrderedCommand> _buffer = new List<OrderedCommand>();
+
+        public StupidReOrderBufferDemo(TimeSpan duration) : base(duration)
+        {
+
+        }
+
+        protected override bool _Flush()
+        {
+            this._buffer.Sort(new OrderedCommandComparer());
+
+            int index = 0;
+            foreach(var x in this._buffer)
+            {
+                if (x.Position == index) this.Pop(x);
+                else this.Drop(x, "unknown");
+
+                index++;
+            }
+
+            return true;
+        }
+
+        protected override bool _Push(OrderedCommand data)
+        {
+            this._buffer.Add(data);
             return true;
         }
     }
-
-
-    public class DemoReOrderBuffer : ReOrderBufferBase
-    {
-        private SortedSet<int> _buffer = new SortedSet<int>();
-
-        public DemoReOrderBuffer(int bufferSize) : base(bufferSize)
-        {
-
-        }
-
-        public override bool Push(int data)
-        {
-            if (data < this._current_index)
-            {
-                // drop
-                return false;
-            }
-            else if (data == this._current_index)
-            {
-                this.Pop(this._current_index++);
-                while (this._buffer.Min == this._current_index)
-                {
-                    this._buffer.Remove(this._current_index);
-                    this.Pop(this._current_index++);
-                }
-
-                // return current & follow sequence
-                return true;
-            }
-            else if (this._buffer.Count > this._buffer_size_limit)
-            {
-                for (; this._current_index < this._buffer.Min; this._current_index++)
-                {
-                    this.Skip(this._current_index);
-                }
-                while (this._buffer.Min == this._current_index)
-                {
-                    this._buffer.Remove(this._current_index);
-                    this.Pop(this._current_index++);
-                }
-
-                //this.Skip(data);
-                //while (this._buffer.Min == this._current_index)
-                //{
-                //    this._buffer.Remove(this._current_index);
-                //    this.Pop(this._current_index++);
-                //}
-
-                // out of buffer size, skip 
-                return true;
-            }
-            else
-            {
-                this._buffer.Add(data);
-                return true;
-            }
-        }
-    }
-
 }
